298d8ebb0a8d20cfccf34ac6d1142be4e93e3cf0
konraddissake1808 <konrad.dissakengando@gmail.com>
2024-10-17 11:34:22 +0100

    Upload image to page

--- newsletter-sign-up-page/src/app/page.tsx
+++ newsletter-sign-up-page/src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import SignUpPage from "./signUpPage/page";
 
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
-        
+    <div className="font-[family-name:var(--font-geist-sans)] w-screen h-screen flex justify-center">
+        <div className="w-full h-full">
+          <SignUpPage />
+        </div>
     </div>
   );
 }
---
+++ newsletter-sign-up-page/src/app/signUpPage/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+import SignUpPageImage from '../components/signUpPageImage/page'
+
+function SignUpPage() {
+  return (
+    <div>
+      <SignUpPageImage></SignUpPageImage>
+    </div>
+  )
+}
+
+export default SignUpPage
